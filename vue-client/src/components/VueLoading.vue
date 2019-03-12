7c1195df45ca9d16e1ae93ef6ad3bd6678605ec4
unknown <felixlopezarm1@gmail.com>
2019-03-12 19:57:14 -0400

    :sparkles: vue themed loading gif

---
+++ vue-client/src/components/VueLoading.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="vue-loading">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1184px-Vue.js_Logo_2.svg.png" alt="vue logo">
+      <img src="https://craigshelly.com/skin/frontend/puro/default/images/AjaxLoader.gif" alt="loading gif">
+  </div>
+</template>
+
+<style scoped>
+  #vue-loading{
+    width: 15%;
+    margin: 20px auto;
+  }
+  img{
+    width: 100%;
+    height: auto;
+  }
+
+
+</style>
+
